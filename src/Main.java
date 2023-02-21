972f8e66c44af49b58c4c50127d76975e4c525e0
ArShuranov <arshuranov@yandex.ru>
2023-02-21 22:33:49 +0700

    Homework done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+        System.out.println(task_1());
+        System.out.println();
+        task_2();
+        System.out.println();
+        task_3();
+
     }
+
+    public static String task_1() {
+        String firstName = "Ivan";
+        String middletName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middletName;
+        return("ФИО сотрудника - " + fullName);
+    }
+
+    public static void  task_2() {
+        String firstName = "Ivan";
+        String middletName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middletName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+    }
+    public static void  task_3() {
+        String fullName = "Иванов Семён Семёнович";
+        String result =fullName.replace("ё", "е");
+        System.out.println(result);
+    }
+
 }
